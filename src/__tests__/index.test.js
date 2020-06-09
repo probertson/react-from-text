2d245605e8f9a6aed444fa477e0fb661ff4077ed
Taylor Everding <taylor1791@users.noreply.github.com>
2020-06-09 16:52:04 -0600

    Replace implementation with latest.
    Benefits in no order:
    
     * Improved error messages.
     * Placeholders values support props.
     * Messages can use the same placeholder multiple times.

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -4,171 +4,233 @@ import { shallow } from 'enzyme';
 import fromText from '../index';
 
 describe('fromText', () => {
-  it('substitutes named placeholder variables', () => {
-    const values = {
-      theDate: 'foo',
-    };
-
-    const message = 'Date completed: %(theDate)s';
-    const expected = 'Date completed: foo';
-
-    expect(fromText(message, values)).toEqual(expected);
+  it('substitutes sprintf-style placeholders', () => {
+    const formatted = fromText('Hello, %(name)s!', { name: 'World' });
+    expect(formatted).toEqual('Hello, World!');
   });
 
-  it('substitutes tag-like placeholders', () => {
-    const tags = {
-      span: <span />,
-    };
+  it('substitutes self closing tags', () => {
+    const formatted = shallow(
+      <div>{fromText('Hello, <span/>World!', { span: <span /> })}</div>
+    );
 
-    const message = 'Date completed: <span>Never</span>';
-    const formattedMessage = shallow(
-      <div>{fromText(message, tags)}</div>
-    ).html();
-    const expected = shallow(
+    expect(
+      formatted.equals(
+        <div>
+          Hello, <span />
+          World!
+        </div>
+      )
+    ).toBe(true);
+  });
+
+  it('supports trailing whitespace in self-closing tags', () => {
+    const formatted = shallow(
+      <div>{fromText('Hello, <span />World!', { span: <span /> })}</div>
+    );
+
+    expect(
+      formatted.equals(
+        <div>
+          Hello, <span />
+          World!
+        </div>
+      )
+    ).toBe(true);
+  });
+
+  it('supports numbers in self closing tags', () => {
+    const formatted = shallow(
+      <div>{fromText('Hello, <break2 />World!', { break2: <br /> })}</div>
+    );
+
+    expect(
+      formatted.equals(
+        <div>
+          Hello, <br />
+          World!
+        </div>
+      )
+    ).toBe(true);
+  });
+
+  it('substitutes empty tag pairs', () => {
+    const formatted = shallow(
+      <div>{fromText('Hello, <span></span>World!', { span: <span /> })}</div>
+    );
+
+    expect(
+      formatted.equals(
+        <div>
+          Hello, <span></span>
+          World!
+        </div>
+      )
+    ).toBe(true);
+  });
+
+  it('treats nested text as strings', () => {
+    const formatted = shallow(
       <div>
-        Date completed: <span>Never</span>
+        {fromText('Hello, <span>World!</span>', {
+          span: <span />,
+        })}
       </div>
-    ).html();
+    );
 
-    expect(formattedMessage).toEqual(expected);
+    expect(
+      formatted.equals(
+        <div>
+          Hello, <span>World!</span>
+        </div>
+      )
+    ).toBe(true);
   });
 
-  it('substitutes tag-like placeholders (multiple uses of one placeholder), giving them unique keys', () => {
-    const tags = {
-      span: <span />,
-    };
-
-    const message = 'Date completed: <span>Never</span> <span>Again</span>';
-    const formattedMessageSpans = shallow(
-      <div>{fromText(message, tags)}</div>
-    ).find('span');
-
-    expect.hasAssertions();
-
-    formattedMessageSpans.forEach(formattedSpan => {
-      expect(
-        formattedMessageSpans.findWhere(
-          spanToCheck => spanToCheck.key() === formattedSpan.key()
-        ).length
-      ).toEqual(1);
-    });
-  });
-
-  it('substitutes self-closing tag-like placeholders', () => {
-    const tags = {
-      br: <br />,
-    };
-
-    const message = 'Date completed: <br/>Never';
-    const formattedMessage = shallow(
-      <div>{fromText(message, tags)}</div>
-    ).html();
-    const expected = shallow(
+  it('maintains props on placeholders', () => {
+    const formatted = shallow(
       <div>
-        Date completed: <br />
-        Never
+        {fromText('Hello, <span>World</span>!', {
+          span: <span style={{ fontWeight: 'bold' }} />,
+        })}
       </div>
-    ).html();
+    );
 
-    expect(formattedMessage).toEqual(expected);
+    expect(
+      formatted.equals(
+        <div>
+          Hello, <span style={{ fontWeight: 'bold' }}>World</span>!
+        </div>
+      )
+    ).toBe(true);
   });
 
-  it('substitutes self-closing tag-like placeholders (multiple uses of one placeholder), giving them unique keys', () => {
-    const tags = {
-      br: <br />,
-    };
-
-    const message = 'Date completed: <br/>Never<br/>Again';
-    const formattedMessageBRTags = shallow(
-      <div>{fromText(message, tags)}</div>
-    ).find('br');
-
-    expect.hasAssertions();
-
-    formattedMessageBRTags.forEach(formattedBR => {
-      expect(
-        formattedMessageBRTags.findWhere(
-          brToCheck => brToCheck.key() === formattedBR.key()
-        ).length
-      ).toEqual(1);
-    });
-  });
-
-  it('substitutes named placeholder variables *and* tag-like placeholders', () => {
-    const values = {
-      theDate: 'foo',
-      span: <span />,
-    };
-
-    const message = 'Date completed: <span>%(theDate)s</span>';
-
-    const formattedMessage = shallow(
-      <div>{fromText(message, values)}</div>
-    ).html();
-    const expected = shallow(
+  it('substitutes sibling tags', () => {
+    const formatted = shallow(
       <div>
-        Date completed: <span>foo</span>
+        {fromText('<div>Hello</div>, <span>World</span>!', {
+          span: <span />,
+          div: <div />,
+        })}
       </div>
-    ).html();
+    );
 
-    expect(formattedMessage).toEqual(expected);
+    expect(
+      formatted.equals(
+        <div>
+          <div>Hello</div>, <span>World</span>!
+        </div>
+      )
+    ).toBe(true);
   });
 
-  it('properly handles named placeholder variables that include tag characters', () => {
-    const values = {
-      theName: 'foo < >',
-      span: <span />,
-    };
-
-    const message = 'Position title: <span>%(theName)s</span>';
-
-    const formattedMessage = shallow(
-      <div>{fromText(message, values)}</div>
-    ).html();
-    const expected = shallow(
+  it('substitutes both twin tags', () => {
+    const formatted = shallow(
       <div>
-        Position title: <span>foo &lt; &gt;</span>
+        {fromText('<span>Hello</span>, <span>World</span>!', {
+          span: <span />,
+        })}
       </div>
-    ).html();
+    );
 
-    expect(formattedMessage).toEqual(expected);
+    expect(
+      formatted.equals(
+        <div>
+          <span>Hello</span>, <span>World</span>!
+        </div>
+      )
+    ).toBe(true);
   });
 
-  it('properly handles string wrapped in react container', () => {
-    const values = {
-      span: <span />,
-    };
-
-    const message = '<span>Sup</span>';
-
-    const formattedMessage = shallow(
-      <div>{fromText(message, values)}</div>
-    ).html();
-    const expected = shallow(
+  it('substitutes named sprintf placeholders inside tag characters', () => {
+    const formatted = shallow(
       <div>
-        <span>Sup</span>
+        {fromText('Hello, <span>%(name)s!</span>', {
+          name: 'World',
+          span: <span />,
+        })}
       </div>
-    ).html();
+    );
 
-    expect(formattedMessage).toEqual(expected);
+    expect(
+      formatted.equals(
+        <div>
+          Hello, <span>World!</span>
+        </div>
+      )
+    ).toBe(true);
   });
 
-  it('properly handles just a react node', () => {
-    const values = {
-      span: <span />,
-    };
-
-    const message = '<span></span>';
-
-    const formattedMessage = shallow(
-      <div>{fromText(message, values)}</div>
-    ).html();
-    const expected = shallow(
+  it('substitutes nested tags', () => {
+    const formatted = shallow(
       <div>
-        <span></span>
+        {fromText('<span><span>Hello</span>, World!</span>', {
+          span: <span />,
+        })}
       </div>
-    ).html();
+    );
 
-    expect(formattedMessage).toEqual(expected);
+    expect(
+      formatted.equals(
+        <div>
+          <span>
+            <span>Hello</span>, World!
+          </span>
+        </div>
+      )
+    ).toBe(true);
+  });
+
+  it('substitutes nested self closing', () => {
+    const formatted = shallow(
+      <div>
+        {fromText('Hello, <span><br/>World!</span>', {
+          span: <span />,
+          br: <br />,
+        })}
+      </div>
+    );
+
+    expect(
+      formatted.equals(
+        <div>
+          Hello,{' '}
+          <span>
+            <br />
+            World!
+          </span>
+        </div>
+      )
+    ).toBe(true);
+  });
+
+  it('treats invalid tags as text', () => {
+    const formatted = shallow(<div>{fromText('<Hello World>', {})}</div>);
+
+    expect(formatted.equals(<div>&lt;Hello World&gt;</div>)).toBe(true);
+  });
+
+  it('informs if they are missing a placeholder tag', () => {
+    expect(() => fromText('<div>Hello World!</div>', {})).toThrow(
+      /missing placeholder value <div>/
+    );
+  });
+
+  it('informs if placeholder is not an Element', () => {
+    expect(() => fromText('<div>Hello World!</div>', { div: 1 })).toThrow(
+      /is not a react element/
+    );
+  });
+
+  it('informs if they are missing a closing tag', () => {
+    expect(() => fromText('<div>Hello World!', { div: <div /> })).toThrow(
+      /missing closing tag <\/div>/
+    );
+  });
+
+  it('informs if they use the wrong closing tag', () => {
+    expect(() =>
+      fromText('<span>Hello</div>, World!', { span: <span /> })
+    ).toThrow(/missing closing tag <\/span>/);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { sprintf } from 'sprintf-js';
-import formatChildrenBase from './base-format-children';
+import invariant from 'invariant';
 
 /**
  * Formats a localized string with substituted elements (values and React components).
@@ -13,57 +13,189 @@ import formatChildrenBase from './base-format-children';
  *                     or `<br/>` or `<myReactComponent/>`) the key matching the tag name
  *                     defines the React component that replaces the tag(s).
  */
-export default function reactFromText(message, placeholders) {
-  return replacePlaceholders(
-    formatChildrenBase(applyChildren, message, placeholders),
-    placeholders
-  );
+export default function fromText(message, placeholders) {
+  const context = { string: message };
+
+  const sprintfed = sprintf(message, placeholders);
+  const ast = parseFragment(sprintfed, context);
+  const children = substituteTags(ast, placeholders, context);
+
+  return children.length === 1 ? children[0] : children;
 }
 
-function replacePlaceholders(segment, placeholders, dedupeKeySuffix) {
-  // it's a string so just substitute the placeholders
-  if (typeof segment === 'string') {
-    return sprintf(segment, placeholders);
+function parseFragment(rawMessage, context) {
+  let message = rawMessage;
+  const result = [];
+
+  while (message !== '') {
+    const { remaining, ...props } = parseNext(message, context);
+
+    result.push({ ...props });
+    message = remaining;
   }
 
-  // it's an array of (strings and/or React elements) so perform replacement on each one
-  if (Array.isArray(segment)) {
-    return segment.map((subSegment, index) =>
-      replacePlaceholders(subSegment, placeholders, index.toString())
-    );
-  }
-
-  // it's a React element, so perform replacement on its child(ren) instead
-  if (segment.props.children) {
-    const replacedChildren = replacePlaceholders(
-      segment.props.children,
-      placeholders
-    );
-    return React.cloneElement.apply(
-      React,
-      [
-        segment,
-        { key: dedupeKeySuffix ? segment.key + dedupeKeySuffix : segment.key },
-      ].concat(replacedChildren)
-    );
-  }
-
-  // it's a React element without any children, so just return the node
-  return dedupeKeySuffix
-    ? React.cloneElement.apply(React, [
-        segment,
-        { key: segment.key + dedupeKeySuffix },
-      ])
-    : segment;
+  return result;
 }
 
-function applyChildren(key, element, children) {
-  if (process.env.NODE_ENV !== 'production' && !React.isValidElement(element)) {
-    throw new Error(JSON.stringify(element) + ' is not a valid element');
+function parseNext(message, context) {
+  let { kind, value, name, remaining } = parseNextToken(message);
+
+  if (kind === TOKENS.SELF) {
+    return {
+      kind: NODES.TAG,
+      value: { name, children: null },
+      remaining,
+    };
+  } else if (kind === TOKENS.OPEN) {
+    const next = parseUntilClosing(name, remaining, context);
+    invariant(next, createError(`missing closing tag </${name}>`, context));
+
+    const { children, remaining: newRemaining } = next;
+
+    return {
+      kind: NODES.TAG,
+      value: {
+        name,
+        children,
+      },
+      remaining: newRemaining,
+    };
+  } /* kind === TOKENS.TEXT */ else {
+    return { kind: NODES.TEXT, value: value, remaining: remaining };
+  }
+}
+
+function parseNextToken(message) {
+  const match = message.match(/\<\/?(\w+?)(\s*\/)?\>/);
+
+  // Text until tag
+  if (match && match.index !== 0) {
+    const value = message.substr(0, match.index);
+    const remaining = message.substr(match.index);
+
+    return {
+      kind: TOKENS.TEXT,
+      value,
+      remaining,
+    };
   }
 
-  return React.cloneElement.apply(
-    React,
-    [element, { key: element.key || key }].concat(children || [])
-  );
+  // Tag
+  if (match && match.index === 0) {
+    const { name, kind } = getTagInfo(match[0]);
+    const remaining = message.substr(match[0].length);
+
+    return { kind, name, remaining };
+  }
+
+  // Only text remains
+  return { kind: TOKENS.TEXT, value: message, remaining: '' };
+}
+
+function getTagInfo(string) {
+  const openingTag = string.match(/\<(\w+)\>/i);
+  const closingTag = string.match(/\<\/(\w+)\>/i);
+  const selfClosingTag = string.match(/\<(\w+)\s*\/\>/i);
+
+  if (openingTag) {
+    return { name: openingTag[1], kind: TOKENS.OPEN };
+  }
+
+  if (closingTag) {
+    return { name: closingTag[1], kind: TOKENS.CLOSE };
+  }
+
+  /* selfClosingTag */
+  return { name: selfClosingTag[1], kind: TOKENS.SELF };
+}
+
+function parseUntilClosing(tagName, message, context) {
+  const children = [];
+
+  do {
+    const { name, value, kind, remaining } = parseNextToken(message);
+
+    if (kind === TOKENS.CLOSE) {
+      invariant(
+        tagName === name,
+        createError(`missing closing tag </${tagName}>`, context)
+      );
+
+      return { children, remaining };
+    } else if (kind === TOKENS.SELF) {
+      message = remaining;
+      children.push({
+        kind: NODES.TAG,
+        value: {
+          name,
+          children: null,
+        },
+      });
+    } else if (kind === TOKENS.OPEN) {
+      const {
+        children: nestedChildren,
+        remaining: nestedRemaining,
+      } = parseUntilClosing(name, remaining, context);
+
+      message = nestedRemaining;
+      children.push({
+        kind: NODES.TAG,
+        value: { name, children: nestedChildren },
+      });
+    } /* kind === TOKENS.TEXT */ else {
+      message = remaining;
+      children.push({
+        kind: NODES.TEXT,
+        value,
+      });
+    }
+  } while (message !== '');
+}
+
+const NODES = {
+  TEXT: 'TEXT',
+  TAG: 'TAG',
+};
+
+const TOKENS = {
+  TEXT: 'TEXT',
+  SELF: 'SELF',
+  OPEN: 'OPEN',
+  CLOSE: 'CLOSE',
+};
+
+function substituteTags(message, placeholders, context) {
+  return message.map((node, i) => {
+    if (node.kind === NODES.TAG) {
+      const { name, children } = node.value;
+
+      invariant(
+        placeholders.hasOwnProperty(name),
+        createError(`missing placeholder value <${name}>`, context)
+      );
+
+      const props = { key: `${name}-${i}` };
+      const element = placeholders[name];
+
+      invariant(
+        React.isValidElement(element),
+        createError(
+          `placeholder value <${name}> is not a react element`,
+          context
+        )
+      );
+      return React.cloneElement(
+        placeholders[name],
+        props,
+        ...(children ? substituteTags(children, placeholders, context) : [])
+      );
+    }
+
+    /* node.kind === NODES.TEXT */
+    return node.value;
+  });
+}
+
+function createError(error, context) {
+  return [`fromText: ${error}`, `Source: ${context.string}`].join('\n');
 }
